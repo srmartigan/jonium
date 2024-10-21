a88816b0ccec8f311c671a8d32937a6942b72efa
Jonatan Iglesias <srmartigan@gmail.com>
2024-10-21 20:18:00 +0200

    Añade y mejora lógica en ORM y PostController
    Se incorporan métodos en ORM para manejar búsquedas y pluralización de modelos. En PostController, se ajustan consultas y visualización de datos de usuario. Además, se asegura que el nombre no sea NULL en la migración de usuarios.

--- Migrations/UserMigrations.php
+++ Migrations/UserMigrations.php
@@ -11,11 +11,12 @@ class UserMigrations
     {
         DB::runQuery('
     CREATE TABLE users (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);'
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);'
         );
     }
 
